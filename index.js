8f774831ba3b10d676a5d5df0ef816b21eabc5c0
willemv <willemv@synaq.com>
2017-10-12 09:23:19 +0200

    Broken out IngoToZimbraConverter class into a separate file

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 #!/usr/bin/env node
 'use strict';
-
 const application = require("commander");
 const mysql = require('nodejs-mysql').default;
 const phpSerializer = require("serialize-like-php");
 const IngoToZimbraRuleConverter = require('./src/IngoToZimbraRuleConverter');
-
 const converter = new IngoToZimbraRuleConverter(application, mysql, phpSerializer);
 converter.initialiseApplication();
