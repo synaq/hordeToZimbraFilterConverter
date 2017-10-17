ba6d3196f37cb1ded43444dda3e8d9cc5e51abe1
willemv <willemv@synaq.com>
2017-10-17 09:14:27 +0200

    IngoToZimbraConverter, when valid rules are returned, ignores unsupported "greater than or equal to" conditions

--- tests/IngoToZimbraConverterTest.js
+++ tests/IngoToZimbraConverterTest.js
@@ -665,6 +665,36 @@ describe('IngoToZimbraConverter', () => {
             expect(process.stdout.write).to.have.been.calledWith('afrl "The Rule" active any address "From" all is "bar@bar-less-equal.com"  keep  \n');
         });
 
+        it('ignores unsupported "greater than or equal to" conditions', () => {
+            returnedRules = [
+                {
+                    action: '1',
+                    'action-value': null,
+                    combine: '2',
+                    conditions: [
+                        {
+                            field: 'From',
+                            match: 'greater than or equal to',
+                            value: 'foo@foo.com'
+                        },
+                        {
+                            field: 'From',
+                            match: 'is',
+                            value: 'bar@bar-greater-equal.com'
+                        }
+                    ],
+                    name: 'The Rule',
+                    stop: null
+                }
+            ];
+            phpSerializer.unserialize = () => {
+                return returnedRules;
+            };
+            converter.initialiseApplication();
+            // noinspection JSUnresolvedVariable
+            expect(process.stdout.write).to.have.been.calledWith('afrl "The Rule" active any address "From" all is "bar@bar-greater-equal.com"  keep  \n');
+        });
+
         it('remaps "begins with" matches to "contains"', () => {
             returnedRules = [
                 {
